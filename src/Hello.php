9be35c20862b924c4cc5eba2129bdd247c998252
shenxinyu <shenxinyu@3ncto.com>
2021-05-25 17:24:33 +0800

    'composer'

---
+++ src/Hello.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Hello;
+
+class Hello{
+	public static function world(){
+		echo 'hello word sxy';
+	}
+}
---
+++ src/P.php
@@ -0,0 +1,7 @@
+<?php
+if (!function_exists('p')) {
+    function p($str) {
+       
+        echo $str;
+    }
+}
